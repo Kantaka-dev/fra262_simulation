a1c9193dea64eeeb8cff08360d348c5ba4e7657c
Kantaka-dev <64575382+Kantaka-dev@users.noreply.github.com>
2021-03-27 13:27:35 +0700

    v1.0
    First Demo
    task
      - insert speed limit
      - add substation (at least 4)

--- Module3Simulation.py
+++ Module3Simulation.py
@@ -21,10 +21,13 @@ IMAGE = {
 
 # Color
 COLOR = {
-    'WHITE' : (255, 255, 255),
-    'ORANGE': (255, 167, 76),
-    'RED'   : (255, 108, 70),
-    'BLUE'  : (70, 205, 255)
+    'WHITE'     : (255, 255, 255),
+    'ORANGE'    : (255, 167, 76),
+    'RED'       : (255, 108, 70),
+    'BLUE'      : (70,  205, 255),
+    'YELLOW'    : (255, 208, 54),
+    'YELLOW1'   : (223, 182, 49),
+    'BLACK'     : (32,  34,  41)
 }
 
 # Font
@@ -46,18 +49,18 @@ class InputBox:
     def draw(self, mode=1):
         # Mode:0
         if mode == 0:
-            pg.draw.rect(screen, (255,255,255), (self.x, self.y, self.w, self.h))
-            screen.blit(FONT(20).render(self.value, True, (0,0,0)), (self.x+5, self.y+5))
+            pg.draw.rect(screen, COLOR['BLACK'], (self.x, self.y, self.w, self.h))
+            screen.blit(FONT(28).render(self.value, True, COLOR['WHITE']), (self.x+80, self.y+10))
             return
         # Box
-        if self.able:
+        elif self.able:
             pg.draw.rect(screen, (195,195,195), (self.x, self.y, self.w, self.h))
         elif self.isMouseOn():
             pg.draw.rect(screen, (220,220,220), (self.x, self.y, self.w, self.h))
         else:
             pg.draw.rect(screen, (255,255,255), (self.x, self.y, self.w, self.h))
         # Font
-        screen.blit(FONT(20).render(self.value, True, (0,0,0)), (self.x+5, self.y+5))
+        screen.blit(FONT(28).render(self.value, True, (0,0,0)), (self.x+80, self.y+10))
 
     def handleEvent(self, event):
         # Click
@@ -81,7 +84,7 @@ class InputBox:
         return True if self.x < mouse_x < self.x+self.w and self.y < mouse_y < self.y+self.h else False
 
 class Simulation:
-    def __init__(self, input_box):
+    def __init__(self, input_box, max_rpm=10):
         self.input_box = input_box
         # Plotting variable
         self.alpha = 0.5        # rad/s^2
@@ -95,6 +98,8 @@ class Simulation:
         self.total_distance = 0.0   # deg
         self.total_time = 0         # frames
         self.timer = 0              # frames
+        # Requirement
+        self.omega_limit = max_rpm /30 *math.pi # rad/s
 
     def init(self):
         screen.blit(IMAGE['BACKGROUND'], (0, 0))
@@ -149,13 +154,14 @@ class Simulation:
         # print("total time: {:.2f} s".format(self.total_time/FPS))
         #  omega_max = sqrt(2*alpha*theta)
         self.omega_max = math.sqrt(self.alpha*DtoR(float(self.input_box.value)))
+        
+        screen.blit(IMAGE['BACKGROUND'], (0, 0))
 
     def runSimulation(self):
         self.init_simu()
         print("\n===== Simulation =====")
         print("initial position: {:.3f} deg".format(self.theta[1]))
         self.run_simu = True
-        screen.blit(IMAGE['BACKGROUND'], (0, 0))
         
         while self.run_simu:
             screen.blit(IMAGE['BACKGROUND_L'], (0, 0))
@@ -188,7 +194,8 @@ class Simulation:
 
             CLOCK.tick(FPS)
 
-    def drawEndEffector(self, mode=0):
+    def drawEndEffector(self, mode=0, linkstyle=7):
+        link_length = [self.radius*math.cos(DtoR(self.theta[0])), self.radius*math.sin(DtoR(self.theta[0]))]
         # Mode:1
         if mode == 1:
             if self.timer == self.total_time//2: # half way after
@@ -199,15 +206,26 @@ class Simulation:
             self.theta[0] += RtoD(self.omega)/FPS # deg/s * s/frame
             
             # End-effector currunt position
-            self.end_effector = [self.center[0]+self.radius*math.cos(DtoR(self.theta[0])), 
-                                 self.center[1]+self.radius*math.sin(DtoR(self.theta[0]))]
+            self.end_effector = [self.center[0] + link_length[0], self.center[1] + link_length[1]]
+        
         # Mode:2
         elif mode == 2:
             # End-effector final position
             self.theta[0] = self.total_distance
-            self.end_effector = [self.center[0]+self.radius*math.cos(DtoR(self.theta[0])), 
-                                 self.center[1]+self.radius*math.sin(DtoR(self.theta[0]))]
+            self.end_effector = [self.center[0] + link_length[0], self.center[1] + link_length[1]]
         
+        # draw link
+        # (version1)
+        # pg.draw.line(screen, COLOR['YELLOW'], self.center, (int(self.end_effector[0]), int(self.end_effector[1])), 30)
+        # (version2)
+        for i in range(1, linkstyle+1):
+            pg.draw.circle(screen, COLOR['BLACK'], (
+                int(self.end_effector[0] -(i/linkstyle)* link_length[0]),
+                int(self.end_effector[1] -(i/linkstyle)* link_length[1])
+            ), 20)
+        # draw center
+        pg.draw.circle(screen, COLOR['BLACK'], self.center, 37)
+        # draw end-effector
         pg.draw.circle(screen, COLOR['ORANGE'], (int(self.end_effector[0]), int(self.end_effector[1])), 30)
     
     def plotTheta(self, mode=0, begin=(656,640), scale=(240,110)): # begin: origin(x,y), scale: (width,height)
@@ -253,7 +271,7 @@ class Simulation:
         #     pg.draw.line(screen, COLOR['RED'], (begin[0]+scale[0]//2, begin[1]+scale[1]//2), 
         #     (begin[0]+int(self.timer/self.total_time*scale[0]), begin[1]+scale[1]//2), 6)
 
-input_distance = InputBox(230, 680)
+input_distance = InputBox(x=230, y=680)
 
-Simulation = Simulation(input_distance)
+Simulation = Simulation(input_distance, max_rpm=10)
 Simulation.run()
